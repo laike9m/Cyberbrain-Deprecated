dbb8e20b4a173724d694ec3d437028b0fbf055d5
laike9m <laike9m@gmail.com>
2019-07-14 18:05:02 -0700

    Added flow.py

---
+++ cyberbrain/flow.py
@@ -0,0 +1,9 @@
+"""Execution flow that represents a program's execution."""
+
+
+class Node:
+    """Node that forms the execution flow."""
+
+
+class CallNode(Node):
+    """Node that represents a callsite."""
