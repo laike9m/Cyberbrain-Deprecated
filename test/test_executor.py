b7683d66882084245ccbb12a312a708cd98f6dc1
laike9m <laike9m@gmail.com>
2019-07-15 22:22:50 -0700

    again, fix Windows

--- test/test_executor.py
+++ test/test_executor.py
@@ -19,7 +19,9 @@ def run_scripts_and_compare():
         print(previous)  # Shows scripts output.
 
         with open(expected_output, "r") as f:
-            assert json_text == f.read().replace("\r\n", "\n").strip("\r\n")
+            assert json_text == f.read().replace("\r\n", "\n").replace(
+                r"\r\n", r"\n"
+            ).strip("\r\n")
 
     return runner
 
