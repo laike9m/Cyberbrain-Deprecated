08db10dc45b62307ef3e40625d5ac050d4eaa1a8
laike9m <laike9m@gmail.com>
2019-07-21 02:04:08 -0700

    Removed Line and Call, only use Node

--- cyberbrain/flow.py
+++ cyberbrain/flow.py
@@ -1,13 +1,35 @@
 """Execution flow that represents a program's execution."""
 
+import ast
 import typing
+
+import astor
 
 from .utils import ID
 from . import backtrace
 
 
 class Node:
-    """Base class."""
+    """Basic unit of an execution flow."""
+
+    def __init__(
+        self,
+        *,
+        code_str: str = None,
+        code_ast: ast.AST = None,
+        data: typing.Dict,
+        arg_to_param: typing.Dict[ID, ID] = None
+    ):
+        if not any([code_str, code_ast]):
+            raise ValueError("Should provide code_str or code_ast.")
+        self.code_str = code_str or astor.to_source(code_ast)
+        self.code_ast = code_ast or backtrace.parse_code_str(code_str)
+        self.prev = None
+        self.next = None
+        self.step_into = None
+        self.returned_from = None
+        # For simplicity, uses a dict to represent data for now.
+        self.data = data
 
     def build_relation(self, **relation_dict: typing.Dict[str, "Node"]):
         """A convenient function to add relations at once.
@@ -21,44 +43,12 @@ class Node:
             setattr(self, relation_name, node)
 
 
-class Line(Node):
-    """Node that forms the execution flow."""
-
-    def __init__(self, code_str: str, data: typing.Dict):
-        self.code_str = code_str
-        self.code_ast = backtrace.parse_code_str(code_str)
-        self.prev = None
-        self.next = None
-        # For simplicity, uses a dict to represent data for now.
-        self.data = data
-
-
-class Call(Node):
-    """Node that represents a callsite."""
-
-    def __init__(
-        self, code_str: str, arg_to_param: typing.Dict[ID, ID], data: typing.Dict
-    ):
-        self.code_str = code_str
-        self.callsite_ast = backtrace.parse_code_str(code_str)
-        self.arg_to_param = arg_to_param
-        self.prev = None
-        self.next = None
-        self.step_into = None
-        self.returned_from = None
-        self.data = data
-
-
 class Flow:
     """Class that represents program's execution.
 
     A flow consists of multiple Calls and Nodes.
     """
 
-    def __init__(self):
-        self.start = None
-        self.target = None
-
-    def add(self):
-        """Adds a node to flow."""
-        pass
+    def __init__(self, start: Node, target: Node):
+        self.start = start
+        self.target = target
